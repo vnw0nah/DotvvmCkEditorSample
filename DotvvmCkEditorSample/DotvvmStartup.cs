2bf0999cd679cd3c7d71e86f1d0d424bbfe8a6e0
Vincent Nwonah <vnwonah@outlook.com>
2023-05-18 08:32:13 +0100

    Sample CkEditor Repo Complete

---
+++ DotvvmCkEditorSample/DotvvmStartup.cs
@@ -0,0 +1,75 @@
+﻿using DotVVM.Contrib.CkEditorMinimal;
+using DotVVM.Framework.Configuration;
+using DotVVM.Framework.ResourceManagement;
+using DotVVM.Framework.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotvvmCkEditorSample
+{
+    public class DotvvmStartup : IDotvvmStartup, IDotvvmServiceConfigurator
+    {
+        // For more information about this class, visit https://dotvvm.com/docs/tutorials/basics-project-structure
+        public void Configure(DotvvmConfiguration config, string applicationPath)
+        {
+
+            ConfigureRoutes(config, applicationPath);
+            ConfigureControls(config, applicationPath);
+            ConfigureResources(config, applicationPath);
+        }
+
+        private void ConfigureRoutes(DotvvmConfiguration config, string applicationPath)
+        {
+            config.RouteTable.Add("Default", "", "Views/Default.dothtml");
+            config.RouteTable.AutoDiscoverRoutes(new DefaultRouteStrategy(config));
+        }
+
+        private void ConfigureControls(DotvvmConfiguration config, string applicationPath)
+        {
+            // register code-only controls and markup controls
+            config.AddContribCkEditorMinimalConfiguration();
+
+        }
+
+
+        private void ConfigureResources(DotvvmConfiguration config, string applicationPath)
+        {
+            // register custom resources and adjust paths to the built-in resources
+            config.Resources.Register("bootstrap-css", new StylesheetResource
+            {
+                Location = new UrlResourceLocation("~/lib/bootstrap/css/bootstrap.min.css")
+            });
+            config.Resources.Register("bootstrap", new ScriptResource
+            {
+                Location = new UrlResourceLocation("~/lib/bootstrap/js/bootstrap.min.js"),
+                Dependencies = new[] { "bootstrap-css", "jquery" }
+            });
+            config.Resources.Register("jquery", new ScriptResource
+            {
+                Location = new UrlResourceLocation("~/lib/jquery/jquery.min.js")
+            });
+            config.Resources.Register("Styles", new StylesheetResource()
+            {
+                Location = new UrlResourceLocation("~/Resources/style.css")
+            });
+
+            config.Resources.Register("ckeditor", new ScriptResource(
+      new UrlResourceLocation("~/lib/ckeditor/ckeditor.js"))
+            {
+                Dependencies = new[] { "jquery" }
+            });
+
+            config.Resources.Register("ckeditor-config", new ScriptResource(
+                        new UrlResourceLocation("~/lib/ckeditor/config.js"))
+            {
+                Dependencies = new[] { "ckeditor" }
+            });
+
+        }
+
+        public void ConfigureServices(IDotvvmServiceCollection options)
+        {
+            options.AddDefaultTempStorages("temp");
+            options.AddHotReload();
+        }
+    }
+}
---
+++ DotvvmCkEditorSample/ViewModels/DefaultViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+
+namespace DotvvmCkEditorSample.ViewModels
+{
+    public class DefaultViewModel : MasterPageViewModel
+    {
+
+		public string Title { get; set;}
+		public string HtmlText { get; set;}
+
+		public DefaultViewModel()
+		{
+			Title = "Hello from DotVVM!";
+			HtmlText = "Hello TextBox";
+		}
+
+    }
+}
